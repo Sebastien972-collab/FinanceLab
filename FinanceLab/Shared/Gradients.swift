c2146ad055c69d398af1443514ecb4a242b7ed36
Anne Ferret <anne@anneferret.eu>
2025-09-25 15:53:53 +0200

    project colors

---
+++ FinanceLab/Shared/Gradients.swift
@@ -0,0 +1,42 @@
+//
+//  Gradients.swift
+//  FinanceLab
+//
+//  Created by Anne Ferret on 25/09/2025.
+//
+
+import SwiftUI
+
+// Call gradients in your project like in the following exemple :
+//
+// Rectangle()
+//     .fill(LinearGradient.redGradient)
+
+extension LinearGradient {
+    static let redGradient = LinearGradient(
+        colors: [
+            Color.Gradient.Red.start,
+            Color.Gradient.Red.stop,
+        ],
+            startPoint: .top,
+            endPoint: .bottom
+        )
+    
+    static let greenGradient = LinearGradient(
+        colors: [
+            Color.Gradient.Green.start,
+            Color.Gradient.Green.stop,
+        ],
+        startPoint: .top,
+        endPoint: .bottom
+    )
+    
+    static let primaryGradient = LinearGradient(
+        colors: [
+            Color.Gradient.Primary.start,
+            Color.Gradient.Primary.stop,
+        ],
+        startPoint: .top,
+        endPoint: .bottom
+    )
+}
