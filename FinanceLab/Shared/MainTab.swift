6144092550fcfd64e9bc24317e1cc4dca845c5b6
Sébastien DAGUIN <sebastien.daguin94@gmail.com>
2025-09-25 15:18:22 +0200

    initial commit

---
+++ FinanceLab/Shared/MainTab.swift
@@ -0,0 +1,32 @@
+//
+//  ContentView.swift
+//  FinanceLab
+//
+//  Created by Sébastien Daguin on 25/09/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    private enum Selection {
+        case home, project, inform
+    }
+    @State private var selection: Selection = .home
+    var body: some View {
+        TabView {
+            Tab("Mon budget", systemImage: "wallet.bifold.fill") {
+                Text("Home")
+            }
+            Tab("Mes Projets", systemImage: "powermeter") {
+                Text("Projets")
+            }
+            Tab("Ressources", systemImage: "newspaper.fill") {
+                Text("S'informer")
+            }
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
