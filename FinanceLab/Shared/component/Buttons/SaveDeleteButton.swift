e8b50543a45e8b9664892ca63cad7c7a239bc974
DEMBO DIAOUNE <diaouned@yahoo.fr>
2025-09-26 14:50:26 +0200

    ajout bouton save delete

---
+++ FinanceLab/Shared/component/Buttons/SaveDeleteButton.swift
@@ -0,0 +1,33 @@
+//
+//  SaveDeleteButton.swift
+//  FinanceLab
+//
+//  Created by Dembo on 26/09/2025.
+//
+
+import SwiftUI
+
+struct SaveDeleteButton: View {
+    var title: String
+    var action: () -> Void
+    
+    var body: some View {
+        Button(action: action) {
+            Text(title)
+                .font(.headline)
+                .fontWeight(.semibold)
+                .foregroundColor(.white)
+                .frame(width: 120, height: 44)
+                .background(LinearGradient.greenGradient)
+                .cornerRadius(30)
+        }
+        .padding(.horizontal)
+    }
+}
+
+#Preview {
+    SaveDeleteButton(
+        title: "Enregistrer",
+        action: {}
+    )
+}
