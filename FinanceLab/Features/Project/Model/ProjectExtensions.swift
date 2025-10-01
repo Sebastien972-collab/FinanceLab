3a549d14f19884ea1319f67b447cf43e2c25519e
Sébastien DAGUIN <sebastien.daguin94@gmail.com>
2025-10-01 16:17:41 +0200

    Structure initial du projet

---
+++ FinanceLab/Features/Project/Model/ProjectExtensions.swift
@@ -0,0 +1,13 @@
+//
+//  ProjectExtensions.swift
+//  FinanceLab
+//
+//  Created by Sébastien Daguin on 30/09/2025.
+//
+
+import Foundation
+import FinanceCore
+
+extension Project {
+    
+}
---
+++ FinanceLab/Features/Project/ViewModel/ProjectViewModel.swift
@@ -0,0 +1,25 @@
+//
+//  ProjectViewModel.swift
+//  FinanceLab
+//
+//  Created by Sébastien Daguin on 30/09/2025.
+//
+
+import Foundation
+import FinanceCore
+
+@Observable
+final class ProjectViewModel {
+    private(set) var projects: [Project] = []
+    func fetchProjects() {
+    }
+    
+    func add(_ project: Project) {
+        guard !projects.contains(project) else { return }
+    }
+    func remove(_ project: Project) {
+        guard projects.contains(project) && else { return }
+    }
+
+    
+}
