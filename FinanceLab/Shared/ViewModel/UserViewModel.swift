63c3768950ef0d3a7ac3a590e4bb6904b1aab346
Sébastien DAGUIN <sebastien.daguin94@gmail.com>
2025-10-14 16:50:21 +0200

    Modification de l'arborescence des fichiés et des noms

---
+++ FinanceLab/Shared/ViewModel/UserViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  UserViewModel.swift
+//  FinanceLab
+//
+//  Created by Sébastien DAGUIN on 14/10/2025.
+//
+
+import SwiftUI
+
+struct UserViewModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UserViewModel()
+}
