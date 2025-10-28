3fa6db60e1b93bf4db67ccc9fe9829ecc3914a38
Anne Ferret <anne@anneferret.eu>
2025-10-28 16:50:36 +0100

    SpendingRepartition only shows transactions for current month

--- FinanceLab/Features/Accounting/ViewModels/AccountViewModel.swift
+++ FinanceLab/Features/Accounting/ViewModels/AccountViewModel.swift
@@ -66,9 +66,18 @@ class AccountViewModel {
     }
     
     func calcSpendingRepartition() {
+        // Gets only the transactions from current month
+        let currentMonth = Calendar.current.component(.month, from: Date())
+        let currentYear = Calendar.current.component(.year, from: Date())
+        let monthlyTransactions = transactionsList.filter { transac in
+            let transacMonth = Calendar.current.component(.month, from: transac.date)
+            let transacYear = Calendar.current.component(.year, from: transac.date)
+            return transacMonth == currentMonth && transacYear == currentYear
+        }
+
         // Return the sum of all negative transactions in the array (as an absolute)
         spent = 0
-        for transac in transactionsList {
+        for transac in monthlyTransactions {
             if transac.amount < 0 {
                 spent += -transac.amount
             }
@@ -76,7 +85,7 @@ class AccountViewModel {
         
         // Return the sum of all positive transactions in the array (as an absolute)
         gained = 0
-        for transac in transactionsList {
+        for transac in monthlyTransactions {
             if transac.amount > 0 {
                 gained += transac.amount
             }
